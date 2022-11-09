97c6f3498a0155eb0f338b4aa693f4a2bb4b21e7
Mark Noonan <mark@cypress.io>
2022-11-08 21:50:42 -0500

    add cypress and examples

---
+++ src/components/HelloWorld.cy.js
@@ -0,0 +1,9 @@
+import HelloWorld from './HelloWorld.vue'
+
+describe('<HelloWorld />', () => {
+  it('renders', () => {
+    // see: https://test-utils.vuejs.org/guide/
+    cy.mount(HelloWorld)
+    cy.contains('hi')
+  })
+})
